d1f54bcfc13787ebe7011352f2d4df6942da4ab1
PARIJAT DUTTA <84469154+parijatdutta1@users.noreply.github.com>
2024-01-10 10:27:25 +0530

    Update download.php

--- download.php
+++ download.php
@@ -1,1 +1,14 @@
+<?php
 
+$file = $GET['resume'] .".pdf" ;
+
+header("content-disposition: attachment; filename=" .urlencode($resume));
+
+$fb = fopen($file, "r");
+
+while(!feof($fb)){
+  echo fread($fb, 8192);
+  flush();
+}
+fclose($fb);
+?>
